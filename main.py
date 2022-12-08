b54caeb040eacac008b06169c99bed851cb909c1
Frollamma <fral01@protonmail.com>
2022-12-08 17:24:25 +0100

    Fixes

--- main.py
+++ main.py
@@ -2,69 +2,37 @@ import numpy as np
 #import sympy as sp
 from utils import *
 from sympy.matrices import Matrix
-from sympy import Rational, pprint
+# from sympy import Rational, pprint
 # from fractions import Fraction
 
 '''
 Takes in input a linear programming problem in standard form
 '''
 
-def first_negative_item_index(my_list):
-    for i in range(len(my_list)):
-        if my_list[i] < 0:
-            return i
-    return -1
 
-def get_base_costs(c, base_indexes):
-    return Matrix([c[i] for i in base_indexes]).transpose()
-
-def compute_tableau(A, b, c, base_indexes):
-    B = A[:, base_indexes]
-    # print('B: ', B)
-    invB = B.inv()
-    x_B = invB * b
-    # print('x_B: ', x_B)
-    c_B = get_base_costs(c, base_idx)
-    # print('c_B: ', c_B)
-    C = invB * A
-    # print('C: ', C)
-    rc = c - c_B * invB * A # Reduced costs
-    # print('rc: ', rc)
-    z = - c_B * x_B
-    # print('z: ', z)
-    return [x_B, z, rc, C]
-    
-def compose_tableau(x_B, z, reduced_costs, C):
-    row = z.row_join(reduced_costs)
-    # print('row: ', row)
-    tableau = x_B.row_join(C)
-    # print('tableau: ', tableau)
-    tableau = row.col_join(tableau)
-    return tableau
-
-def print_tableau(tableau, mode='plain'):
-    '''Mode can be \'plain\' or \'latex\''''
-    if mode=='plain':
-        pprint(tableau)
-    elif mode=='LaTeX':# TEMP - IMPR
-        pass
-
-
-# Matrix of technologic coefficents
+# Matrix of technical coefficents
 A = Matrix([
-    [3, -2, 1, 0],
-    [1, 0, 0, 1]
+    [1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0],
+    [2, 5, -1, 0, 0, 0, 1, 0],
+    [4, 2, 0, -1, 0, 0, 0, 1]
 ])
+# A = Matrix([
+#     [3, -2, 1, 0],
+#     [1, 0, 0, 1]
+# ])
 # A = Matrix([[1, 2, Rational(3, 2)], [4, 5, 6], [7, 23, 5]])
 
 # Right-hand sides
-b = Matrix([12, 5])
+# b = Matrix([12, 5])
+b = Matrix([6, 4, 10, 8])
 
 # Vector of costs (relative to the linear objective function)
-c = Matrix([-3, 2, 0, 0]).transpose()
+#c = Matrix([-1, -1, 0, 0, 0, 0, 0, 0]).transpose()
+c = Matrix([0, 0, 0, 0, 1, 1, 1, 1]).transpose()
 
 # Indexes of A columns corresponding to the base
-base_idx = [2, 3]
+base_idx = [4, 5, 6, 7]
 
 m = A.rows
 n = A.cols
@@ -90,20 +58,21 @@ while counter < max_iterations:
 
     # j = np.where(np.asarray(rc) < 0)[0][0]
     j = first_negative_item_index(rc)
+    # print(rc)
+    print(j)
     if j == -1:
-        x = [x_B[base_idx.index(i)] if i in base_idx else 0 for i in range(n)]
-        # x = []
-        # for i in range(n):
-        #     if i in base_idx:
-        #         x.append(x_B[i])
-        #     else:
-        #         x.append(0)
-        
+        x = [x_B[base_idx.index(i)] if i in base_idx else 0 for i in range(n)]# IMPR: it seems inefficient
         print(f"Found optimal finite solution: {x}")
         break
     else:
-        # print([x_B[k]/A[k, i] for k in range(m)])
-        i = np.argmin([x_B[k]/A[k, j] for k in range(m)])# Am I sure there's no division by zero? - CHECK
+        # print([x_B[k]/A[k, j] for k in range(m)])
+        # print([x_B[k]/A[k, j] for k in range(m) if A[k, j] != 0])
+        # # i = np.argmin([x_B[k]/A[k, j] for k in range(m) if A[k, j] != 0])     # This is not the solution because gets the wrong index
+        # i = np.argmin([x_B[k]/A[k, j] for k in range(m)])# Division by zero can happen and cause problems... - CHECK
+
+        i = argmin_of_fractions(x_B, A[:, j])
+        if i == -1:         # This should never happen
+            raise Exception("Found null column")
 
         print(f"x_{j + 1} enters the base, x_{base_idx[i] + 1} exits the base")
         print(base_idx)
--- utils.py
+++ utils.py
@@ -1,9 +1,55 @@
 import numpy as np
-# import sympy as sp
+from sympy.matrices import Matrix
+from sympy import pprint
 
-def is_tolerable(value, tolerance=10**(-7)):
-    return (abs(value) <= tolerance).all()
+# Generic
+## Math
+def first_negative_item_index(my_list):
+    for i in range(len(my_list)):
+        if my_list[i] < 0:
+            return i
+    return -1
 
+def argmin_of_fractions(numerators, denominators):
+    # We assume numerators are positive
+    n = len(numerators)
+    if n != len(denominators):
+        raise ValueError("Iterables with different lenghts")
+    
+    i = 0
+    while denominators[i] == 0 and i < n:
+        i += 1
+    
+    if i == n:
+        # This means that every denominator is 0
+        return -1
+
+    current_min = numerators[i]/denominators[i]
+    for k in range(i + 1, n):
+        if denominators[i] != 0:
+            possible_min = numerators[i]/denominators[i]
+            if possible_min < current_min:
+                i = k
+                current_min = possible_min
+    return i
+                
+
+
+## Numerical approximation (I think I'll delete this...)
+# def is_tolerable(value, tolerance=10**(-7)):
+#     return (abs(value) <= tolerance).all()
+
+# def is_solution_approx(A, b, x):
+#     return is_tolerable(np.dot(A, x) - b)
+
+# def is_feasible_solution_approx(A, b, x):
+#     return (x >= 0).all() and is_solution_approx(A, b, x)
+
+# def is_basic_solution_approx(A, b, x, tol=10**(-7)):
+#     return is_solution_approx(A, b, x) and True# TEMP! - CHECK L. 2 p. 41
+
+
+# Linear Programming Problem theory
 def is_standard_form(A, b, c, A_rank = None):# IMPR
     # At thee moment I get input in the form of the triple (A, b, c), so I can perform few checks for now. In the future I'll get input in a different form (more similar to real problems) and I'll have to run more checks - IMPR
     # Checks if b has got all nonnegative elements
@@ -21,7 +67,7 @@ def is_standard_form(A, b, c, A_rank = None):# IMPR
 
     # At the moment, I can't check if each element of b is nonnegative directly with sympy... - IMPR (I used .applyfunction but this kind of use is deprecated)
     # print(b)
-    b = np.asarray(list(b))
+    b = np.asarray(list(b))# IMPR
     # print(b)
     # print(b >= 0)
 
@@ -30,19 +76,49 @@ def is_standard_form(A, b, c, A_rank = None):# IMPR
 
     return A_rank == A.rows and no_zero_columns and no_zero_rows and (b >= 0).all()
 
-#    return A_rank == A.rows and not no_zero_columns and not no_zero_rows and np.all(b >= 0)
 
 def is_solution(A, b, x):
-    return is_tolerable(np.dot(A, b) - x)
+    '''Checks if x is the solution to the linear system Ax = b'''
+    return A*x == b
 
 def is_feasible_solution(A, b, x):
-    return (x >= 0).all() and is_solution(A, b, x)
+    return (np.asarray(list(x)) >= 0).all() and is_solution(A, b, x) # IMPR
 
-# def is_basic_solution(A, b, x, tol=10**(-7)):
-#     return is_solution(A, b, x) and True# TEMP! - CHECK L. 2 p. 41
-
+# def is_basic_solution_approx(A, b, x):
+    # pass
 # def is_basic_feasible_solution(A, b, x):
-#     return is_solution(A, b, x) and (x >= 0).all() and
+    # pass
+
+
+# Full tableau Method
+def get_base_costs(c, base_indexes):
+    return Matrix([c[i] for i in base_indexes]).transpose()
+
+def compute_tableau(A, b, c, base_indexes):
+    B = A[:, base_indexes]
+    invB = B.inv()
+    x_B = invB * b
+    c_B = get_base_costs(c, base_indexes)
+    C = invB * A
+    rc = c - c_B * invB * A # Reduced costs
+    z = - c_B * x_B
+    
+    return [x_B, z, rc, C]
+    
+def compose_tableau(x_B, z, reduced_costs, C):
+    row = z.row_join(reduced_costs)
+    tableau = x_B.row_join(C)
+    tableau = row.col_join(tableau)
+    
+    return tableau
+
+def print_tableau(tableau, mode='plain'):
+    '''Mode can be \'plain\' or \'latex\''''
+    if mode=='plain':
+        pprint(tableau)
+    elif mode=='LaTeX':# TEMP - IMPR
+        pass
+
 
 
 
